f029c7fdf82ddc788df7098c0e8957b65fe68df1
Christina Milner <christina@christina-milner.dev>
2023-02-09 18:52:35 +0000

    Added todo and buttons

--- public/main.js
+++ public/main.js
@@ -1,3 +1,14 @@
+/* To do:
+- Add CSS to make it look not horrible
+- Add default listing of all elements on page
+- Add "edit entry" button
+    - Clicking an entry should then open the form for editing it and allow doing so
+    - In editing view, there also needs to be the option to add prizes (which are not a thing on entry creation)
+- Add functionality to filter by prizes
+- Change add entry functionality so multiple people can't create an entry with the same id at the same time
+*/
+
+
 const addButton = document.querySelector('#addButton')
 
 addButton.addEventListener('click', openAddForm)
